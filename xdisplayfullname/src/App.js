08b750bfe40019f91ea045854cc36975dc185d80
Ronit Roushan <ronit21102@gmail.com>
2024-04-27 12:41:02 +0530

    change

--- xdisplayfullname/src/App.js
+++ xdisplayfullname/src/App.js
@@ -1,25 +1,25 @@
 import { useRef, useState } from "react";
 
 function App() {
+   const [fullName, setFullName] = useState('');
    
-   const [fullName,setFullName] = useState('');
-   const firstName =useRef()
-   const lastName =useRef()
-   
-   const addName =(e)=>{
+   const firstName = useRef();
+   const lastName = useRef();
 
-     setFullName(`${firstName.current.value} ${lastName.current.value}`)
+   const addName = (e) => {
+     e.preventDefault();
+     setFullName(`${firstName.current.value} ${lastName.current.value}`);
    }
    
   return (
-    <>
+    <form onSubmit={addName}>
        <h1>Full Name Display</h1>
-       <span>First Name:</span><input type="text" ref={firstName}/><br/>
-       <span>Last Name:</span><input type='text' ref={lastName} /><br/>
-        <button onClick={addName}>Submit</button>
-
-        {fullName.length>0&&<p>Full Name:{fullName}</p>}
-    </>
+       <span>First Name:</span><input type="text" ref={firstName} required /><br/>
+       <span>Last Name:</span><input type='text' ref={lastName} required /><br/>
+       <button type="submit">Submit</button>
+       
+       {fullName && <p>Full Name: {fullName}</p>}
+    </form>
   );
 }
 
